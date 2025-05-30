c475488e618517efdfe53b3ea242beb2d76da49d
Navarro Rangel <navarrorangel1@hotmail.com>
2025-05-30 10:59:40 -0300

    Chore: Include DTO version by entity and map manually at NinjaMapper the ways entity x dto

--- src/main/java/java10x/dev/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/java10x/dev/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package java10x.dev.CadastroDeNinjas.Ninjas;
 
+
+import java10x.dev.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
+
 }
