63f4f02c1fbd5ff8fa2b1d404ae0953844282e72
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-05 10:17:58 -0300

    add: NinjaMapper added to map ninjaDTO

--- src/main/java/java10x/dev/CadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/java10x/dev/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,4 +1,15 @@
 package java10x.dev.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        return missoesModel;
+    }
 }
